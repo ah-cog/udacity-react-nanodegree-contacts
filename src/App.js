a268127c705586a7c4f82d287201971e760b1a4f
Michael Gubbels <mgub@mgub.co>
2017-10-06 19:03:22 -0700

    Integrated frontend into backend server.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts }) // same as { contacts: contacts }
+    })
   }
   removeContact = (contact) => {
     this.setState((state) => ({
@@ -38,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
